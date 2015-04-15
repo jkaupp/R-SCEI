242a142148245d0e48be54198d2aadcb0a5ff728
Jake Kaupp <jkaupp@gmail.com>
2015-04-15 19:06:25 -0400

    Using Pander for Tables

--- R/CEEA 2015 Analysis.R
+++ R/CEEA 2015 Analysis.R
@@ -2,26 +2,27 @@ library(xlsx)
 library(dplyr)
 library(magrittr)
 library(lubridate)
-library(xtable)
+library(pander)
 
 # Read in the data
-data<-read.xlsx("/Users/Jake/ownCloud/R/Projects/R-SCEI/data/iterim_national_data.xlsx", 1)
-names<-read.xlsx("/Users/Jake/ownCloud/R/Projects/R-SCEI/data/iterim_national_data.xlsx", 2)
+survey_data<-read.xlsx("/Users/Jake/ownCloud/R/Projects/R-SCEI/data/iterim_national_data.xlsx", 1)
+var_names<-read.xlsx("/Users/Jake/ownCloud/R/Projects/R-SCEI/data/iterim_national_data.xlsx", 2)
 
 # Set the names of the data to the modified headers
-data %<>%
-  set_names(names$modified.header)
+survey_data %<>%
+  set_names(var_names$modified.header)
 
 # Fix the duration to decimal minutes
-old_time <- data$time %>% 
+old_time <- survey_data$time %>% 
   ymd_hms 
 
 new_time <- minute(old_time) + second(old_time)/60
-data$time <- new_time
+survey_data$time <- new_time
 
-# Calculate mean completion time
-data$time %>% 
-  mean
-
+survey_data$collector %>% 
+  table %>% 
+  as.data.frame %>%
+  set_names(c("Institution","Responses")) %>% 
+  pander
 
 
